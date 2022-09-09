6517ff78fea8c04f0e39d8a74628910676af0a2f
Alex <kozlov.alex.dev@gmail.com>
2022-09-09 12:25:34 +0300

    ✅ kata done

--- src/main/java/org/bubblegumsharkie/codewars/kyu4/RomanNumerals/RomanNumerals.java
+++ src/main/java/org/bubblegumsharkie/codewars/kyu4/RomanNumerals/RomanNumerals.java
@@ -1,10 +1,9 @@
 package org.bubblegumsharkie.codewars.kyu4.RomanNumerals;
 
-import java.util.ArrayList;
 import java.util.TreeMap;
 
 public class RomanNumerals {
-    private final static TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+    private final static TreeMap<Integer, String> map = new TreeMap<>();
 
     static {
         map.put(1000, "M");
@@ -21,43 +20,66 @@ public class RomanNumerals {
         map.put(4, "IV");
         map.put(1, "I");
     }
+
     public static String toRoman(int n) {
-        int l =  map.floorKey(n);
-        if ( n == l ) {
+        int l = map.floorKey(n);
+        if (n == l) {
             return map.get(n);
         }
-        return map.get(l) + toRoman(n-l);
+        return map.get(l) + toRoman(n - l);
     }
 
     public static int fromRoman(String romanNumeral) {
-        ArrayList<Character> romanNumeralChar = new ArrayList<>();
-        romanNumeralChar.add(' ');
-        for (char c : romanNumeral.toCharArray()
-        ) {
-            romanNumeralChar.add(c);
-        }
-        romanNumeralChar.add(' ');
-        int year = 0;
-        for (int i = 0; i < romanNumeralChar.size(); i++) {
-            if (romanNumeralChar.get(i) == 'M') year += 1000;
-            if (romanNumeralChar.get(i) == 'D') year += 500;
-            if (romanNumeralChar.get(i) == 'C') year += 100;
-            if (romanNumeralChar.get(i) == 'L') year += 50;
-            if (romanNumeralChar.get(i) == 'X' && romanNumeralChar.get(i - 1) != 'I') {
-                year += 10;
-            } else if (romanNumeralChar.get(i) == 'X' && romanNumeralChar.get(i - 1) == 'I') {
-                year += 9;
+        int decimal = 0;
+        int lastNumber = 0;
+        for (int x = romanNumeral.length() - 1; x >= 0; x--) {
+            char convertToDecimal = romanNumeral.charAt(x);
+
+            switch (convertToDecimal) {
+                case 'M':
+                    decimal = processDecimal(1000, lastNumber, decimal);
+                    lastNumber = 1000;
+                    break;
+
+                case 'D':
+                    decimal = processDecimal(500, lastNumber, decimal);
+                    lastNumber = 500;
+                    break;
+
+                case 'C':
+                    decimal = processDecimal(100, lastNumber, decimal);
+                    lastNumber = 100;
+                    break;
+
+                case 'L':
+                    decimal = processDecimal(50, lastNumber, decimal);
+                    lastNumber = 50;
+                    break;
+
+                case 'X':
+                    decimal = processDecimal(10, lastNumber, decimal);
+                    lastNumber = 10;
+                    break;
+
+                case 'V':
+                    decimal = processDecimal(5, lastNumber, decimal);
+                    lastNumber = 5;
+                    break;
+
+                case 'I':
+                    decimal = processDecimal(1, lastNumber, decimal);
+                    lastNumber = 1;
+                    break;
             }
-            if (romanNumeralChar.get(i) == 'V' && romanNumeralChar.get(i - 1) != 'I') year += 5;
-            if (romanNumeralChar.get(i) == 'V' && romanNumeralChar.get(i - 1) == 'I') year += 4;
-            if (romanNumeralChar.get(i) == 'I' && romanNumeralChar.get(i - 1) == 'V' && romanNumeralChar.get(i + 1) != 'V') {
-                year += 1;
-            } else if (romanNumeralChar.get(i) == 'I' && romanNumeralChar.get(i - 1) != 'V'&& romanNumeralChar.get(i + 1) != 'X') {
-                year += 1;
-            }
         }
-        return year;
+        return decimal;
     }
 
-
+    public static int processDecimal(int decimal, int lastNumber, int lastDecimal) {
+        if (lastNumber > decimal) {
+            return lastDecimal - decimal;
+        } else {
+            return lastDecimal + decimal;
+        }
+    }
 }
