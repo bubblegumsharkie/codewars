5210799e5ca536f87e233383d9c1119a1ee190cd
Alex <kozlov.alex.dev@gmail.com>
2022-11-06 19:44:46 +0300

    ✅ kata done (new Tests impl)

---
+++ src/main/java/org/bubblegumsharkie/codewars/kyu6/createphonenumber/Kata.java
@@ -0,0 +1,9 @@
+package org.bubblegumsharkie.codewars.kyu6.createphonenumber;
+
+import java.util.stream.IntStream;
+
+public class Kata {
+    public static String createPhoneNumber(int[] numbers) {
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d", IntStream.of(numbers).boxed().toArray());
+    }
+}
